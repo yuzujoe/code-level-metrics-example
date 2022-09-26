f319e6c2ea9f2fb430bcbb47eae1053b4730b78f
yuzujoe <yuzun225@gmail.com>
2022-09-27 00:10:40 +0900

    first commit

---
+++ go.mod
@@ -0,0 +1,16 @@
+module code-level-metrics-example
+
+go 1.19
+
+require (
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/newrelic/go-agent/v3 v3.19.2 // indirect
+	github.com/newrelic/go-agent/v3/integrations/nrgorilla v1.1.1 // indirect
+	golang.org/x/net v0.0.0-20220923203811-8be639271d50 // indirect
+	golang.org/x/sys v0.0.0-20220919091848-fb04ddd9f9c8 // indirect
+	golang.org/x/text v0.3.7 // indirect
+	google.golang.org/genproto v0.0.0-20220923205249-dd2d53f1fffc // indirect
+	google.golang.org/grpc v1.49.0 // indirect
+	google.golang.org/protobuf v1.28.1 // indirect
+)
---
+++ main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"github.com/gorilla/mux"
+	"github.com/newrelic/go-agent/v3/integrations/nrgorilla"
+	"github.com/newrelic/go-agent/v3/newrelic"
+	"net/http"
+	"os"
+	"time"
+)
+
+func makeNewRelicApplication() (*newrelic.Application, error) {
+	app, err := newrelic.NewApplication(
+		newrelic.ConfigAppName("newrelic-zap-logs-in-context"),
+		newrelic.ConfigLicense(os.Getenv("NEW_RELIC_LICENSE_KEY")),
+		newrelic.ConfigAppLogEnabled(false),
+		newrelic.ConfigCodeLevelMetricsEnabled(true),
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	if err = app.WaitForConnection(5 * time.Second); err != nil {
+		return nil, err
+	}
+
+	return app, nil
+}
+
+func exampleHandler(w http.ResponseWriter, r *http.Request) {
+	txn := newrelic.FromContext(r.Context())
+	defer txn.StartSegment("ExampleHandler").End()
+
+	w.WriteHeader(http.StatusOK)
+}
+
+func main() {
+	r := mux.NewRouter()
+
+	app, err := makeNewRelicApplication()
+	if err != nil {
+		os.Exit(1)
+	}
+
+	r.Use(nrgorilla.Middleware(app))
+
+	r.HandleFunc(newrelic.WrapHandleFunc(app, "/example", exampleHandler))
+
+	http.ListenAndServe(":8080", r)
+}
